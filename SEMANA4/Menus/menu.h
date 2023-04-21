f8fc6ccd7960dbaf0df73427ef90cd6e2787c8f0
Ricardo.Vaca <ricardo.vaca@udla.edu.ec>
2023-04-20 22:27:40 -0500

    Semana_4

---
+++ SEMANA4/Menus/menu.h
@@ -0,0 +1,2 @@
+int menu_principal();
+char menu_producto();
---
+++ SEMANA4/Modulos/modulos.h
@@ -0,0 +1,5 @@
+float aplicar_descuento(float subtotal);
+void imprimir_factura(float numa, float numb, float numc, float numd, float nume,
+                      float prca, float prcb, float prcc, float prcd, float prce,
+                      float tota, float totb, float totc, float totd, float tote,
+                      char nombre[], char cedula[]);
